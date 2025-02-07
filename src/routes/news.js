caddb642b7f59c08d31b9fd345952498bad37e66
anhdovn <ducanh.fcds.gtvt@gmail.com>
2025-02-07 22:11:44 +0700

    setup prettier

--- src/routes/news.js
+++ src/routes/news.js
@@ -2,7 +2,7 @@ const newsController = require('../app/controllers/NewsController');
 const express = require('express');
 const router = express.Router();
 
-	router.use('/:slug', newsController.show);
+router.use('/:slug', newsController.show);
 router.use('/', newsController.index);
 
 module.exports = router; // Export router to use in another file. ( nodejs_blog/src/routes/index.js )
